26534fe2da93907db7cd83b2df738769e6d10433
hyojeong00 <gywjd9563@naver.com>
2020-07-14 15:05:59 +0900

    0714

---
+++ 20200714/1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+class Car
+{
+    //속성(attribute) - 변수 등으로 표현
+    public int Speed; //속도:숫자 =>int
+    private const int SpeedValue = 10;
+    private const int SpeedLimit = 200;
+
+    //행위(behavior) - 메소드로 표현
+    public void Run()
+    {
+        Console.WriteLine("달립니다.");
+    }
+    
+    public void Accel()
+    {
+        Speed = Speed + SpeedValue;
+        if(Speed>SpeedLimit)
+        {
+            Speed = SpeedLimit;
+        }
+        Console.WriteLine("현재 속도는 {0}입니다.", Speed);
+    }
+
+    public void Break()
+    {
+        Speed = Speed - SpeedValue;
+        if(Speed<0)
+        {
+            Speed = 0;
+        }
+        Console.WriteLine("현재 속도는 {0}입니다.", Speed);
+    }
+}
+class Auto
+{
+    static void Main(string[] args)
+    {
+        Car aCar=new Car();
+        aCar.Run();
+        aCar.Accel();
+        aCar.Accel();
+        aCar.Accel();
+
+    }
+    static void Main1(string[] args)
+    {
+        //Car aCar;
+        //aCar = new Car(); //객체생성
+        //aCar.Run();
+        //aCar.Speed = 0;
+        //Console.WriteLine("aCar의 속도는 {0}입니다.", aCar.Speed);
+        //Console.WriteLine("aCar의 속도는 " + aCar.Speed + "입니다.");
+    }
+
+}
---
+++ 20200714/2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        class Car //이너클래스
+        {
+            string color;
+            string vender;
+            string name;
+
+            /*생성자 만들기
+             * 1.메소드다
+             * 2.클래스와 이름이 동일
+             * 3.반환 타입이 존재하지 않는다
+             */
+
+            public Car(string name)  //생성자 오버로딩
+            {
+                Console.WriteLine("Car 생성자 호출");
+            }
+
+            public Car() //디폴트 생성자
+            {
+                Console.WriteLine("Car 디폴트 생성자 호출");
+            }
+
+            ~Car()
+            {
+                Console.WriteLine("Car 소멸자 호출");
+                //종료할때, 메모리가 모자랄때, 할일없을때 
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1========================");
+            Car aCar = new Car("비앰다블유");
+            //aCar.Car();
+            new Car();
+            Console.WriteLine("2========================");
+
+        }
+    }
+}
