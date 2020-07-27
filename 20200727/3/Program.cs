f38338e6dd5944eb7b3b278b26197eef9cb6003d
hyojeong00 <gywjd9563@naver.com>
2020-07-27 18:27:19 +0900

    20200727

---
+++ 20200727/3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Params
+{
+    // [DllImport("user32.dll")]
+    [DllImport("KERNEL32.dll")]
+    // static public extern void MessageBeep(int a, int b);
+    static public extern void Beep(int a, int b);
+
+    static int Add(params int[] values)
+    {
+        int result = 0;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            result += values[i];
+        }
+        return result;
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Add(1, 2, 3, 4, 5));
+        Console.WriteLine(Add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        Beep(512, 200);
+        Beep(640, 200);
+        Beep(768, 200);
+    }
+}
