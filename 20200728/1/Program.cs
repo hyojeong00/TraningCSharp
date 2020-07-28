702688630ad24b662d48ddda9135f57dd374d517
hyojeong00 <gywjd9563@naver.com>
2020-07-28 18:31:56 +0900

    20200728 ip네트워크통신

---
+++ 20200728/1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            IPAddress ipAddr1 = IPAddress.Parse("192.168.0.156");
+            IPEndPoint endpoint = new IPEndPoint(ipAddr1, 9000);
+
+            IPHostEntry entry = Dns.GetHostEntry("www.google.com");
+            foreach(IPAddress ipAddress in entry.AddressList)
+                {
+                Console.WriteLine(ipAddress);
+                }
+
+            string myComputer = Dns.GetHostName();
+
+            Console.WriteLine("컴퓨터 이름 : " + myComputer);
+
+            IPHostEntry entry1 = Dns.GetHostEntry(myComputer);
+            foreach(IPAddress ipAddress1 in entry1.AddressList)
+            {
+                Console.WriteLine(ipAddress1.AddressFamily + ": " + ipAddress1);
+            }
+
+        }
+    }
+}
---
+++ 20200728/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Server.Bind(new IPEndPoint(IPAddress.Any, 7000));
+        Server.Listen(100);
+
+        Socket Client = Server.Accept();
+        Console.WriteLine("Client Incomming");
+
+        byte[] Buffer = new byte[] { 65, 66, 67, 68 };
+        Client.Send(Buffer); //텔넷이 아스키코드로 해석해서 ABCD로 출력
+
+
+        Client.Close();
+        Server.Close();
+    }
+
+
+
+}
